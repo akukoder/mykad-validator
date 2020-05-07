b86a1851f375df41e5ead131ca182593b90e5d81
Syahril Zulkefli <syahzul@gmail.com>
2020-05-07 20:15:50 +0800

    Added mote test

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -24,32 +24,42 @@ class ValidatorTest extends TestCase
     {
         $this->expectException(InvalidLengthException::class);
 
-        $this->validator->validate('8008081020');
+        $this->validator->validate('8008081020', true);
     }
 
     public function testCharacterException()
     {
         $this->expectException(InvalidCharacterException::class);
 
-        $this->validator->validate('8O0808102000');
+        $this->validator->validate('8O0808102000', true);
     }
 
     public function testDateException()
     {
         $this->expectException(InvalidDateException::class);
 
-        $this->validator->validate('001238-10-2000');
+        $this->validator->validate('001238-10-2000', true);
     }
 
     public function testStateCodeException()
     {
         $this->expectException(InvalidCodeException::class);
 
-        $this->validator->validate('801208-00-2000');
+        $this->validator->validate('801208-00-2000', true);
     }
 
     public function testValidInputException()
     {
         $this->assertTrue($this->validator->validate('801208-06-2000'));
+    }
+
+    public function testValidCheckException()
+    {
+        $this->assertTrue($this->validator->validate('801208-06-2000'));
+    }
+
+    public function testInvalidCheckException()
+    {
+        $this->assertFalse($this->validator->validate('800008-00-2000'));
     }
 }
